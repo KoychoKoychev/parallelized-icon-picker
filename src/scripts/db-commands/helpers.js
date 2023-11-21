9d7c1f4b74a8e58ee54e86100334ef3903fbdd9f
KoychoKoychev <kgkoychev@gmail.com>
2023-11-21 17:47:46 +0200

    feat: initiate db function

---
+++ src/scripts/db-commands/helpers.js
@@ -0,0 +1,44 @@
+
+const db = require('./index.js');
+
+function addIcon(data) {
+    db.run(
+        `INSERT INTO icons (domain, size, dataUri) VALUES (?, ?, ?)`,
+        [data.domain, data.size, data.dataUri],
+        function (error) {
+            if (error) {
+                console.error(error.message);
+            }
+            console.log(`Inserted a row with the ID: ${this.lastID}`);
+        }
+    );
+
+}
+
+function getDomainIcon(domainName) {
+    return new Promise((resolve, reject) => {
+        db.get("SELECT * FROM icons WHERE domain = ?", [domainName], (err, row) => {
+            if (err) {
+                reject(err.message);
+            }
+            resolve(row)
+        })
+    })
+}
+
+function getFullDB() {
+    return new Promise((resolve, reject) => {
+        db.all("SELECT * FROM icons", (err, rows) => {
+            if (err) {
+                reject(err.message);
+            }
+            resolve(rows)
+        })
+    })
+}
+
+module.exports = {
+    addIcon,
+    getDomainIcon,
+    getFullDB
+}
